bb858934edd3af4192b71a8f560e2fc890fa2beb
dilshan <dilshanthisara79@gmail.com>
2022-07-05 10:26:16 +0530

    CreatePdfGenerater

---
+++ src/PDF Generator/PDF.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import './PDF.css';
+
+function PDFgen() {
+    return (
+      <div className="container">
+        <button id="button">Generate PDF</button>
+        <div className="card" id="makepdf">
+        </div>
+      </div>
+    ) }; 
+
+    export default PDF;
